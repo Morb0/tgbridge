68dbeb363e50741ff6534dc3d3f73206f969d921
Morb0 <14136326+Morb0@users.noreply.github.com>
2022-05-03 00:04:43 +0300

    feat: mvp

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Telegram Bridge
-Telegram Bot-to-Bot communication bridge.
+Telegram Bot-to-Bot communication bridge.\
+User-bot resend message received from trusted peer to peer specified in message text payload.
+
+### Example
+- Bot A send message to Bridge with next message `|botB|Lorem Ipsum`.
+- Bridge receive message and extract `botB` login then remove it from message text and send `botB` message `Lorem Ipsum`.
+- Bot B receive message from Bridge `Lorem Ipsum`.
+
+This makes more sense for forwarding files, since each user has a unique ID for uploaded files.
--- src/telegram.service.ts
+++ src/telegram.service.ts
@@ -1,20 +1,79 @@
 import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Api, TelegramClient } from 'telegram';
+import { NewMessage, NewMessageEvent } from 'telegram/events';
 
 @Injectable()
 export class TelegramService implements OnApplicationBootstrap {
   private readonly logger = new Logger(this.constructor.name);
 
-  constructor(private readonly client: TelegramClient) {
-    client.addEventHandler(this.onUpdate.bind(this));
+  constructor(
+    private readonly client: TelegramClient,
+    private readonly configService: ConfigService,
+  ) {
+    client.addEventHandler(this.onNewMessage.bind(this), new NewMessage({}));
   }
 
   async onApplicationBootstrap(): Promise<void> {
     await this.checkCurrentUser();
   }
 
-  private async onUpdate(update: Api.TypeUpdate): Promise<void> {
-    console.log(update);
+  private async onNewMessage(event: NewMessageEvent): Promise<void> {
+    if (!event.isPrivate) {
+      return;
+    }
+
+    const isAllowedPeer = this.configService
+      .get('ALLOWED_PEER_IDS', '')
+      .split(',')
+      .includes(event.chatId.toString());
+    if (!isAllowedPeer) {
+      return;
+    }
+
+    const { message } = event;
+    const [payload, peerId] = message.text.match(/\|(.+?)\|/) ?? [];
+    if (!peerId) {
+      await message.respond({
+        message: 'Payload not found',
+      });
+      return;
+    }
+
+    try {
+      const newPeer = await this.resolvePeer(peerId);
+      const messageCopy = new Api.Message({
+        ...message,
+        message: message.text.replace(payload, ''),
+      });
+      await this.client.sendMessage(newPeer, {
+        message: messageCopy,
+      });
+      this.logger.log(`Successfully resend message to "${peerId}" peer`);
+    } catch (e: any) {
+      this.logger.error(`Failed to send message to "${peerId}" peer`, e?.stack);
+      await message.respond({
+        message: 'Failed to resend message',
+      });
+    }
+  }
+
+  private async resolvePeer(peerId: string): Promise<Api.TypePeer> {
+    try {
+      const { peer } = await this.client.invoke(
+        new Api.contacts.ResolveUsername({
+          username: peerId,
+        }),
+      );
+      return peer;
+    } catch {
+      const { peer } = await this.client.invoke(
+        new Api.contacts.ResolvePhone({
+          phone: peerId,
+        }),
+      );
+      return peer;
+    }
   }
 
   private async checkCurrentUser(): Promise<void> {
